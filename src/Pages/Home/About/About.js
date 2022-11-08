5431cd6131b1ddc5ba561685c207b9738e986d67
sajid365-sr <sajidsorker86@gmail.com>
2022-11-09 02:24:01 +0600

    our service area complete

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,12 +4,12 @@ import parts from '../../../assets/images/about_us/parts.jpg'
 
 const About = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero my-32">
       <div className="hero-content flex-col lg:flex-row">
        <div className="relative w-1/2">
 
        <img src={person} alt='person' className="w-4/5 rounded-lg shadow-2xl" />
-       <img src={parts} alt='person' className="absolute w-3/5 border-8 right-12 top-1/2 rounded-lg shadow-2xl" />
+       <img src={parts} alt='person' className="absolute w-3/5 border-8 border-slate-500 right-12 top-1/2 rounded-lg shadow-2xl" />
 
        </div>
         <div className="w-1/2">
@@ -17,10 +17,10 @@ const About = () => {
           <h1 className="text-5xl font-bold">We are qualified <br/>
            & of experience <br/>
             in this field</h1>
-          <p className="py-6">
+          <p className="py-6 text-slate-600">
           There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
           </p>
-          <p className="pb-6">
+          <p className="pb-6 text-slate-600">
           the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
           </p>
           <button className="btn btn-primary">Get More Info</button>
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,35 @@
+
+import React, { useEffect, useState } from 'react';
+import ServiceCart from './ServiceCart';
+
+const Services = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect( () =>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, []);
+    return (
+        <div>
+            <div className='text-center w-1/2 mx-auto'>
+                <p className="text-orange-500 font-bold text-xl">Service</p>
+                <h2 className='text-5xl font-bold my-5'>Our Service Area</h2>
+                <p className='text-slate-500'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
+                {
+                    services.map(service => <ServiceCart 
+                    key={service._id}
+                    service={service}
+                    ></ServiceCart>)
+                }
+            </div>
+            <div className='text-center mb-10'>
+            <button className='btn btn-outline'>More Services</button>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
