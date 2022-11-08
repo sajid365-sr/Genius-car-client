2aeccd845bffa9a3f08fbdf7cb8d29b5e03fc633
sajid365-sr <sajidsorker86@gmail.com>
2022-11-08 22:21:39 +0600

    basic package setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
-      
+    <div>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </div>
   );
 }
