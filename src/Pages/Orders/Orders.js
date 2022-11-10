3e6f17674a59dcc7e66f41135b7ca451c27130a9
sajid365-sr <sajidsorker86@gmail.com>
2022-11-10 15:16:04 +0600

    inventory done delete operation remain

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,27 +1,44 @@
-
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import OrderRow from "./OrderRow";
 
 const Orders = () => {
+  const { user } = useContext(AuthContext);
+  const [orders, setOrder] = useState([]);
 
-    const {user} = useContext(AuthContext);
-    const [orders, setOrder] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setOrder(data));
+  }, [user?.email]);
 
-
-    useEffect( () =>{
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setOrder(data))
-
-    },[user?.email])
-
-
-
-    return (
-        <div>
-            <h2 className='text-5xl'>You have {orders.length} orders.</h2>
-        </div>
-    );
+  return (
+    <div className="overflow-x-auto w-full my-12">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+              orders.map(order => <OrderRow
+              key={order._id}
+              order={order}
+              ></OrderRow>)
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Orders;
